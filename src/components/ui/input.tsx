459a27c900fc5198b5c7355c85387e1e19ed34d6
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-10-18 12:55:56 +0700

    Update Input component with optional additional classnames

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,16 @@
-export function Input({ type, ...props }: React.ComponentProps<"input">) {
+import { cn } from "@/lib/utils";
+
+export function Input({
+  type,
+  className,
+  ...props
+}: React.ComponentProps<"input">) {
   return (
     <input
-      className="w-full border border-border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary text-sm sm:text-base font-body text-dark-primary placeholder:text-dark-secondary"
+      className={cn(
+        "w-full border border-border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary text-sm sm:text-base font-body text-dark-primary placeholder:text-dark-secondary",
+        className
+      )}
       type={type}
       {...props}
     />
